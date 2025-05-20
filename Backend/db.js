3144dae6a3e3a5270d3af4f2b01cd47190cbbe76
jonyls62 <jonyls623@gmail.com>
2025-05-20 18:53:50 -0300

    cracion de carpetas faltantes y config del pool de la base de datos

--- Backend/db.js
+++ Backend/db.js
@@ -1,7 +1,14 @@
-import mysql from "mysql2/promise"
-import dotenv from "dotenv"
+import { createPool } from 'mysql2/promise';
+import dotenv from 'dotenv';
 
-dotenv.config()
+dotenv.config(); // Ejecutamos dotenv para habilitar el acceso a process.env
 
-const connection = await mysql.createConnection(process.env.DATABASE_URL)
-export default connection
+// Creamos un pool de conexiones a MySQL usando la URL completa de la base de datos
+export const pool = createPool({
+  uri: process.env.DATABASE_URL, 
+
+  //para controlar el pool de conexiones:
+  waitForConnections: true, // Espera si todas las conexiones están en uso en vez de lanzar error
+  connectionLimit: 10,       // Máximo número de conexiones simultáneas en el pool
+  queueLimit: 0              // Número máximo de solicitudes en espera (0 = sin límite)
+});
